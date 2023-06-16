135e51ccf1729839469e3c90b0285ea90a489308
Panagiotis Kapsaliaris <74148504+PanosKapss@users.noreply.github.com>
2023-06-16 23:16:06 +0300

    Calculator.c

--- calculator.c
+++ calculator.c
@@ -1,49 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//--------------------------------Main_Function--------------------------------
 int main(int argc, char *argv[])
 {
-
+    
+    //Value That Saves The Input From User To See If It Will Loop The Program After Every Calculation
     int loop;
 
-    while (loop != 0)
+    //While Answer Different Than 0 Continue Looping
+    while (loop != 0)  
     {
-
+        //Values Initialization
         int x, y;
         int ans;
         int result;
-
-        printf("Give the first number:");
+        
+        //Values Registration x = First Number, y = Second Number
+        printf("Give The First Number: ");
         scanf("%d", &x);
-        printf("Give the second number:");
+        printf("Give The Second Number: ");
         scanf("%d", &y);
-        printf("What you want to do with these 2 numbers(1,2,3,4):");
+        printf("What Do You Want To Do With These 2 Numbers(1 = Sum, 2 = Subtraction, 3 = Multiply, 4 = Division): ");
         scanf("%d", &ans);
-
+        
+        //IF "ans" = 1 Then Do Sum
         if (ans == 1)
         {
             result = x + y;
-            printf("Result is %d\n", result);
+            printf("Result Is %d\n", result);
         }
 
+        //IF "ans" = 2 Then Do Substraction    
         else if (ans == 2)
         {
             result = x - y;
-            printf("Result is %d\n", result);
+            printf("Result Is %d\n", result);
         }
-
+            
+        //IF "ans" = 3 Then Do Multiply
         else if (ans == 3)
         {
             result = x * y;
-            printf("Result is %d\n", result);
+            printf("Result Is %d\n", result);
         }
-
+            
+        //Else Do Division
         else
         {
             result = x / y;
-            printf("Result is %d\n", result);
+            printf("Result Is %d\n", result);
         }
-
-        printf("Dp you want to try again(1=yes/0=no):");
+        
+        //Ask User IF They Want To Calculate Again, IF answer Equals To 1 = Yes, Otherwise 0 = No
+        printf("Do You Want To Try Again?(1 = Yes / 0 = No): ");
         scanf("%d", &loop);
     }
     printf("Program Terminated,Goodbye!!\n");
