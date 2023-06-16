34a67680d97e5bf6a9cfa113896dd361baeac491
PanosKapss <74148504+PanosKapss@users.noreply.github.com>
2023-06-16 19:54:28 +0300

    calculator.c

--- calculator.c
+++ calculator.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[])
+{
+
+    int loop;
+
+    while (loop != 0)
+    {
+
+        int x, y;
+        int ans;
+        int result;
+
+        printf("Give the first number:");
+        scanf("%d", &x);
+        printf("Give the second number:");
+        scanf("%d", &y);
+        printf("What you want to do with these 2 numbers(1,2,3,4):");
+        scanf("%d", &ans);
+
+        if (ans == 1)
+        {
+            result = x + y;
+            printf("Result is %d\n", result);
+        }
+
+        else if (ans == 2)
+        {
+            result = x - y;
+            printf("Result is %d\n", result);
+        }
+
+        else if (ans == 2)
+        {
+            result = x * y;
+            printf("Result is %d\n", result);
+        }
+
+        else
+        {
+            result = x / y;
+            printf("Result is %d\n", result);
+        }
+
+        printf("Dp you want to try again(1=yes/0=no):");
+        scanf("%d", &loop);
+    }
+    printf("Program Terminated,Goodbye!!\n");
+    return 0;
+}
