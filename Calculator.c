5b1374d7a60377fa7f7b6b7a0235237d52b115b3
Panagiotis Kapsaliaris <74148504+PanosKapss@users.noreply.github.com>
2023-06-16 23:25:17 +0300

    Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//--------------------------------Main_Function--------------------------------
+// --------------------------------Main_Function--------------------------------
 int main(int argc, char *argv[])
 {
     
