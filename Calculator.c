de5254d22318f7265dbca2356862d7eaa3b440ea
Panagiotis Kapsaliaris <74148504+PanosKapss@users.noreply.github.com>
2023-06-17 20:09:22 +0300

    Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>    // Library Initialization
+#include <stdlib.h>   // Library Initialization 
 
 // --------------------------------Main_Function--------------------------------
 int main(int argc, char *argv[])
@@ -12,53 +12,53 @@ int main(int argc, char *argv[])
     while (loop != 0)
     {
         // Values Initialization
-        int x, y;
-        int ans;
-        int result;
-        float division;
+        int x, y;          // x = First Number, y = Second Number
+        int ans;           // Answer From User To What Arithmetic Operation Wants To Do
+        int result;        // Result For Sum, Substraction, Muliply
+        float division;    // Result For Division
 
         // Values Registration x = First Number, y = Second Number
         printf("--------------------------\n");
         printf("Give The First Number: ");
-        scanf("%d", &x);
+        scanf("%d", &x);        // Takes From Keyboard The First Value
         printf("--------------------------\n");
         printf("Give The Second Number: ");
-        scanf("%d", &y);
+        scanf("%d", &y);        // Takes From Keyboard The Second Value
         printf("--------------------------\n");
         printf("What Do You Want To Do With These 2 Numbers(1 = Sum, 2 = Subtraction, 3 = Multiply, 4 = Division): ");
-        scanf("%d", &ans);
+        scanf("%d", &ans);      // Takes From Keyboard What User Wants To Do
         printf("--------------------------\n");
 
-        // While "answer" Greater Than 4 or Less Than 1 Ask From User To Give A Valid Input
+        // While "ans" Greater Than 4 or Less Than 1 Ask From User To Give A Valid Input
         while (ans > 4 || ans < 1)
         {
             printf("Wrong Input, Please Try Again!!\n");
             printf("What Do You Want To Do With These 2 Numbers(1 = Sum, 2 = Subtraction, 3 = Multiply, 4 = Division): ");
-            scanf("%d", &ans);
+            scanf("%d", &ans);    // Takes From Keyboard What User Wants To Do
             printf("--------------------------\n");
         }
 
         // IF "ans" = 1 Then Do Sum
         if (ans == 1)
         {
-            result = x + y;
-            printf("Result Is: %d\n", result);
+            result = x + y;    // Take x and y Value, Sum Them And Add Result To "result" Value
+            printf("Result Is: %d\n", result);    // Print Result
             printf("--------------------------\n");
         }
 
         // IF "ans" = 2 Then Do Substraction
         else if (ans == 2)
         {
-            result = x - y;
-            printf("Result Is: %d\n", result);
+            result = x - y;    // Take x and y Value, Substract Them And Add Result To "result" Value
+            printf("Result Is: %d\n", result);    // Print Result
             printf("--------------------------\n");
         }
 
         // IF "ans" = 3 Then Do Multiply
         else if (ans == 3)
         {
-            result = x * y;
-            printf("Result Is: %d\n", result);
+            result = x * y;    // Take x and y Value, Multiply Them And Add Result To "result" Value
+            printf("Result Is: %d\n", result);    // Print Result
             printf("--------------------------\n");
         }
 
@@ -68,34 +68,34 @@ int main(int argc, char *argv[])
             // IF x and y Are Equal With 0 Exit Program, Due To Error
             if (x == 0 && y == 0)
             {
-                printf("\n");
+                printf("\n");          // Change Line
                 printf("--------------------------\n");
                 printf("Cant Divide With 0,Error\n");
                 printf("--------------------------\n");
-                printf("\n");
-                exit(EXIT_FAILURE);
+                printf("\n");          // Change Line
+                exit(EXIT_FAILURE);    // Terminates Program Execution And Returns A Failure Status Code
             }
 
             // Else Output The Result
             else
             {
-                division = x / y;
-                printf("Result Is: %.3f\n", division);
+                division = x / y;    // Take x and y Value, Divide Them And Add Result To "result" Value
+                printf("Result Is: %.3f\n", division);    // Print Result
                 printf("--------------------------\n");
             }
         }
 
         // Ask User IF They Want To Calculate Again, IF answer Equals To 1 = Yes, Otherwise 0 = No
         printf("Do You Want To Try Again?(1 = Yes / 0 = No): ");
-        scanf("%d", &loop);
+        scanf("%d", &loop);        // Takes From Keyboard What User Wants To Do
     }
-    printf("\n");
+    printf("\n");        // Change Line
     printf("--------------------------\n");
     printf("Program Terminated,Goodbye!!\n");
     printf("--------------------------\n");
-    printf("\n");
+    printf("\n");        // Change Line
 
-    exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);        // Terminates Program Execution And Returns A Success Status Code
 
     return 0;
 }
